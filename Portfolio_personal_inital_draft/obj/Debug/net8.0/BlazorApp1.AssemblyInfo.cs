3ec272689645e557514387ad6dee7245a7bebd3a
ashwinadhi <ashwinadhikari3@gmail.com>
2025-01-12 11:49:26 +0545

    vhjhh

--- Portfolio_personal_inital_draft/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ Portfolio_personal_inital_draft/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d43509ab7d203eda74cb423be3026afeae3a601e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cccca3ed717a2ded40ced5f1c27d2ad2db49b365")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

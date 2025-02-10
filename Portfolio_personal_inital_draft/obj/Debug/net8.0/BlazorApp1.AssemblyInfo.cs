a781cbb72ad1f69ae4a63dff20556cbc9a958622
ashwinadhi <ashwinadhikari3@gmail.com>
2025-02-10 13:38:36 +0545

    feat(ui): PopUp while navigating
    email pop up

--- Portfolio_personal_inital_draft/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ Portfolio_personal_inital_draft/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+827c2bc8c00af2673c55bb02ec06826ad846747f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf21ee7a6101fb56ebb945ea15e5531de66be94d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

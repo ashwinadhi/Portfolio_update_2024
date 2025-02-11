06cb0a0fd58121e4a7a726151aaf55f9434e4e70
ashwinadhi <ashwinadhikari3@gmail.com>
2025-02-11 17:30:53 +0545

    feat/button
    Almost all done expect carousel

--- Portfolio_personal_inital_draft/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ Portfolio_personal_inital_draft/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a781cbb72ad1f69ae4a63dff20556cbc9a958622")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f220a21bcc742278c6e02d67f1e2faa09158509a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
